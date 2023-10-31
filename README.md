a68eabea25cee48242e9575529f830ad2391a19e
joelnitta <joelnitta@gmail.com>
2023-10-31 11:40:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# day05-ggplot
+
+Instructions for completing this assignment:
+
+Clone this repo, edit the code in [`day05_homework.R`](day05_homework.R), commit your changes, and push them to the remote.
---
+++ day05_homework.R
@@ -0,0 +1,22 @@
+# DAY 5 HOMEWORK
+#
+# Name:
+#
+# 1. Load the "gapminder" dataset (note that this repo comes
+# with the gapminder.csv file in the data_raw folder)
+
+# 2. First, filter the data only to 1952. Next, make a dot (point) plot that
+# shows the following:
+#  - GDP per capita on the x-axis
+#  - Life expectancy on the y-axis
+#  - Size of each point scaled by population
+#  - Color of each point by continent
+#
+# Hints:
+#   - It may be easier to see the pattern in the data by putting the
+#     x-axis on a log-scale, like we did in class.
+
+# 3. Repeat number 2, but this time plot the data only from 2007.
+
+# 4. How does the pattern you see change between the two plots?
+# Hint: in particular, pay attention to Asia
